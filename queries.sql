8eb7dae8fef4212f5689d500e4f718e4cb93ff0a
Cristian Zambrano <cristian@zamcham.com>
2023-06-20 11:02:37 -0400

    Add explain analyze
    For animal_id and email

--- queries.sql
+++ queries.sql
@@ -167,4 +167,6 @@ GROUP BY s.name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
 
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
 EXPLAIN ANALYZE SELECT * FROM visits_partition_2;
