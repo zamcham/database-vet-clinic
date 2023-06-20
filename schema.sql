c5336d2688478b82ef809b886c312a70d8bb3bfc
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-20 08:58:16 -0600

    Add partition table

--- schema.sql
+++ schema.sql
@@ -47,3 +47,18 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX visits_animal_id_asc ON visits(animal_id ASC);
 CREATE INDEX owners_email_asc ON owners(email ASC);
+
+-- CREATE PARTITION TABLE
+CREATE TABLE visits_partitioned (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT,
+  vet_id INT,
+  date_of_visit DATE,
+  PRIMARY KEY(id, vet_id)
+) PARTITION BY RANGE (vet_id);
+
+CREATE TABLE visits_partition_1 PARTITION OF visits_partitioned FOR VALUES FROM (1) TO (2);
+CREATE TABLE visits_partition_2 PARTITION OF visits_partitioned FOR VALUES FROM (2) TO (3);
+CREATE TABLE visits_partition_3 PARTITION OF visits_partitioned FOR VALUES FROM (3) TO (4);
+CREATE TABLE visits_partition_4 PARTITION OF visits_partitioned FOR VALUES FROM (4) TO (MAXVALUE);
+
